7f119edb3e0a857ce60a1bf03cff5d742c4c1e5f
twrobel <tomasz.wrobel@gmail.com>
2014-02-25 09:28:42 -0500

    Update demo.js

--- public/js/demo/demo.js
+++ public/js/demo/demo.js
@@ -12,32 +12,29 @@ module.factory('instrumentsSchemaService', function($http) {
 module.controller('demoController', function($scope, instrumentsSchemaService) {
         $scope.selectedRow = {};
         $scope.listOfFields = [];
-		
-		
-
-		$scope.getInstrumentFields = function(instrumentType){
-			//$scope.foos = myService.getFoos().then(function(foos) {
-			//	$scope.foos = foos;
-			//});
-			if(schema[instrumentType] == undefined ){
-				instrumentsSchemaService.getInstruments(instrumentType).success(function(instrument){	
-					schema[instrumentType] = instrument;
-				});
-			}
-			 
-			return schema[instrumentType]['fields']
+	$scope.getInstrumentFields = function(instrumentType){
+		if(schema[instrumentType] == undefined ){
+			instrumentsSchemaService.getInstruments(instrumentType).success(function(instrument){	
+				if(instrument['fields'] != undefined){
+					$scope.updateFields(instrument['fields']);
+				}
+			});
 		}
+		return $scope.updateFields(schema[instrumentType]['fields'])
+	}
+	$scope.updateFields() = function(instrumentFields){
+		$scope.listOfFields = [];
+		for(var i = 0; i < instrumentFields.length; i++) {
+                	$scope.listOfFields.push({
+                			pos: instrumentFields[i]['pos'],
+                   			name: instrumentFields[i]['name'],
+                   			value: values[i]
+                	});
+            	}
+	}
         $scope.parsePosition = function() {
-			var values = $scope.position.toLowerCase().split('|');
-            $scope.listOfFields = [];
-			var instrumentFields = $scope.getInstrumentFields(values[0]);
-            for(var i = 0; i < instrumentFields.length; i++) {
-                $scope.listOfFields.push({
-                   pos: instrumentFields[i]['pos'],
-                   name: instrumentFields[i]['name'],
-                   value: values[i]
-                });
-            }
+		var values = $scope.position.toLowerCase().split('|');
+            	$scope.getInstrumentFields(values[0]);
         };
 
         $scope.handleRowSelection = function(row) {
